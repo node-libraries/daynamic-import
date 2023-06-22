dd835413c7ac92462c0800db1bc303cf41f8b0e6
SoraKumo <info@croud.jp>
2023-06-23 08:45:57 +0900

    fix module

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dynamicImport = void 0;
+
+const isCommonJS =
+  typeof module !== "undefined" &&
+  module !== null &&
+  module.constructor &&
+  module.constructor.name === "Module";
+
 const dynamicImport = (path) => {
-  return import(path);
+  if (isCommonJS) return import(path);
+  return Promise.resolve(require(path));
 };
+
 exports.dynamicImport = dynamicImport;
